9082414ef547a6fd2da01d57e49e7e2205b8d308
mayankpadshala <mayankpadshala3012@gmail.com>
2017-05-25 18:52:24 +0530

    JS Modified to Open in New Tab

--- myscript.js
+++ myscript.js
@@ -2,11 +2,14 @@
  * Created by mayank on 25/5/17.
  */
 document.getElementById("target1").onclick = function() {
-    location.href = "https://www.facebook.com/MSDhoni/";
+    // location.href = "https://www.facebook.com/MSDhoni/";
+    window.open("https://www.facebook.com/MSDhoni/","_blank");
 };
 document.getElementById("target2").onclick = function() {
-    location.href = "https://www.instagram.com/msdhoniofficial/?hl=en";
+    // location.href = "https://www.instagram.com/msdhoniofficial/?hl=en";
+    window.open("https://www.instagram.com/msdhoniofficial/?hl=en","_blank");
 };
 document.getElementById("target3").onclick = function() {
-    location.href = "https://twitter.com/msdhoni?lang=en";
+    // location.href = "https://twitter.com/msdhoni?lang=en";
+    window.open("https://twitter.com/msdhoni?lang=en","_blank");
 };
