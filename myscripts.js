d16f4520764a38ac10daca9fe0d7839e42d056f9
mayankpadshala <mayankpadshala3012@gmail.com>
2017-05-25 18:19:45 +0530

    MSD-Tributr-Page

---
+++ myscripts.js
@@ -0,0 +1,12 @@
+/**
+ * Created by mayank on 25/5/17.
+ */
+document.getElementById("target1").onclick = function() {
+    location.href = "https://www.facebook.com/MSDhoni/";
+};
+document.getElementById("target2").onclick = function() {
+    location.href = "https://www.instagram.com/msdhoniofficial/?hl=en";
+};
+document.getElementById("target3").onclick = function() {
+    location.href = "https://twitter.com/msdhoni?lang=en";
+};
